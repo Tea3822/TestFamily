1c0f5a2279d6ba73e56dd8e8d3558a71b9205586
unknown <teodorakarev@gmail.com>
2023-09-07 14:10:29 +0200

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Love My Family");
+        sayLoveYou("Mile");
+        sayLoveMySon("Bojan");
+
+    }
+
+    public static void sayLoveYou(String name){
+        System.out.println("LoveYou " +name);
+    }
+    public static void sayLoveMySon(String name){
+        System.out.println("MySon" +name);
+
+    }
+}
